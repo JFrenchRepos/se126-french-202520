2e5215a9c968f6378809f026c5b2dc44ab8349f3
JFrench <joshuafrench535@gmail.com>
2025-01-06 17:33:45 -0500

    Update test.py
    test

---
+++ practice/test.py
@@ -0,0 +1,16 @@
+# this is a first test of the github repo local-to-remote
+#
+#
+#
+#
+# Hello!
+#
+#
+#
+# HH    HH   II
+# HH    HH   
+# HH    HH   II
+# HHHHHHHH   II
+# HH    HH   II
+# HH    HH   II
+# HH    HH   II
