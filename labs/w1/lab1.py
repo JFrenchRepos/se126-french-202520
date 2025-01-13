2b8816f3d45e7a0fcc0b0379315f83969773e598
JFrench <joshuafrench535@gmail.com>
2025-01-12 19:59:38 -0500

    lab1
    lab1

---
+++ labs/w1/lab1.py
@@ -0,0 +1,80 @@
+#Joshua French
+#SE126.04
+#LAB 1
+#1-12-2025
+
+#PROGRAM PROMPT: This is a program that determines whether a meeting room is in violation of fire regulations regarding the maximum room capacity.
+
+#VARIABLE DICTIONARY
+#name           meeting name
+#people         number of people
+#maxpeople      maximum of allowed people in room
+#diff           maxpeople compared to people
+#res            loop controller for checking if user requires more inputs
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+#FUNCTIONS
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+def difference(name, people, maxpeople):
+    diff =  maxpeople - people
+
+    if diff > 0:
+        print("||", name, "can fit", abs(diff),"more people, and it meets fire regulations. ||")
+        print("")
+    if diff < 0:
+        print("|#|", name, "is over capacity! Remove", abs(diff),"people to meet with fire regulations! |#|")
+        print("")
+    elif diff == 0:
+        print("||", name, "has reached capacity. ||")
+        print("")
+
+def response(res):
+    res = input("Check a room? (Y/N): ").lower()
+    while res != "y" and res != "n":
+            print("###Invalid answer, resubmit###")
+            print("")
+            res = input("Check a room? (Y/N): ").lower()
+
+    while res == "y":
+        print("----------=====CAPACITY=====----------")
+        name = str(input("What is the meeting name?: "))
+        print("")
+
+        people = int(input("How many people will be attending?: "))
+        print("")
+
+        maxpeople = int(input("What is the meeting room's maximum allowed capacity?: "))
+        print("")
+    
+        difference(name, people, maxpeople)
+
+        res = input("Check another room? (Y/N): ").lower()
+        while res != "y" and res != "n":
+            print("###Invalid answer, resubmit###")
+            print("")
+            res = input("Check another room? (Y/N): ").lower()
+
+        print("")
+
+    print("----------=====BACK TO START=====----------")
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+#LOOP START
+#-------------------------------------------------------------------------------------------------------------------------------------------------#
+print("----------=====FIRE REGULATION CHECKER=====----------")
+res = input("Start? (Y/N): ").lower()
+while res != "y" and res != "n":
+            print("###Invalid answer, resubmit###")
+            print("")
+            res = input("Start? (Y/N): ").lower()
+
+while res == "y":
+    response(res)
+    res = input("Start again? (Y/N): ").lower()
+    while res != "y" and res != "n":
+            print("###Invalid answer, resubmit###")
+            print("")
+            res = input("Start again? (Y/N): ").lower()
+
+    print("")
+
+print("----------=====GOODBYE!=====----------")
