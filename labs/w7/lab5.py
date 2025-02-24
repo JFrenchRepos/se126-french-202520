944d55404a97d5aa89e26962b3a6f52dde85632d
JFrench <joshuafrench535@gmail.com>
2025-02-24 14:36:25 -0500

    lab

---
+++ labs/w7/lab5.py
@@ -0,0 +1,136 @@
+#Joshua French
+#SE126.04
+#LAB 5
+#1-30-2025
+
+#PROGRAM PROMPT: Build a personal library search system using the file book_list.csv.
+#IMPORTS-----------------------------------------------------------------------------------------------------------------------------------------------------------
+import csv
+
+def swap(index, listName):
+    temp = listName[index]
+    listName[index] = listName[index + 1]
+    listName[index + 1] = temp
+
+def display(dex):
+    print(f"{'Library Number':10}\t{'Title':<32}\t{'Author':30}\t{'Genre':30}\t{'Pages':5}\t{'Status':10}")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+    for i in dex:
+        print(f"{libraries[i]:10}\t{titles[i]:<32}\t{authors[i]:30}\t{genres[i]:30}\t{pages[i]:5}\t{statuses[i]:10}")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------")
+
+def menu():
+    while True:
+        print("\nLibrary Menu")
+        print("1. Show All Titles")
+        print("2. Search by Title")
+        print("3. Search by Author")
+        print("4. Search by Genre")
+        print("5. Search by Library Number")
+        print("6. Show All Available")
+        print("7. Show All On Loan")
+        print("8. EXIT")
+        menuchoice = input("Enter your choice: ")
+        return menuchoice
+
+#CODE--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+libraries = []
+titles = []
+authors = []
+genres = []
+pages = []
+statuses = []
+
+with open("textfiles/w7/book_list.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        libraries.append(rec[0])
+        titles.append(rec[1])
+        authors.append(rec[2])
+        genres.append(rec[3].title())
+        pages.append(rec[4])
+        statuses.append(rec[5].title())
+
+ans = "y"
+while ans == "y":
+    choice = menu()
+
+    if choice == "1":
+            for i in range(len(titles) - 1):
+                for j in range(len(titles) - 1):
+                    if titles[j] > titles[j + 1]:
+                        swap(j, titles)
+
+            dex = list(range(len(titles)))
+            display(dex)
+    elif choice == "2":
+            for i in range(len(titles) - 1):
+                for j in range(len(titles) - 1):
+                    if titles[j] > titles[j + 1]:
+                        swap(j, titles)
+
+            search = input("Enter the Title you are looking for: ")
+
+            min = 0
+            max = len(titles) - 1
+            mid = int((min + max) / 2 )
+
+            while min < max and search.lower() != titles[mid].lower():
+                if search.lower() < titles[mid].lower():
+                    max = mid - 1
+                else:
+                    min = mid + 1
+
+                mid = int((min + max) / 2 )
+    
+            if search.lower() == titles[mid].lower():
+                print(f"\nFound {search}: ")
+                print(f"{'Library Number':10}\t{'Title':<32}\t{'Author':30}\t{'Genre':30}\t{'Pages':5}\t{'Status':10}")
+                print(f"\n--------------------------------------------------------------------------------------------------------------------------------------------")
+                print(f"{libraries[mid]:10}\t{titles[mid]:<32}\t{authors[mid]:30}\t{genres[mid]:30}\t{pages[mid]:5}\t{statuses[mid]:10}")
+                print("--------------------------------------------------------------------------------------------------------------------------------------------\n")
+            else:
+                print("No results found!")
+    elif choice == "3":
+            found = []
+            search = input("Enter the Author you are looking for: ")
+            for i in range(0, len(authors)):
+                if search.lower() in authors[i].lower():
+                    found.append(i)
+
+            if not found:
+                print("No results found!")
+            else:
+                print(f"\nFound {search}: ")
+                print(f"{'Library Number':10}\t{'Title':<32}\t{'Author':30}\t{'Genre':30}\t{'Pages':5}\t{'Status':10}")
+                print(f"\n--------------------------------------------------------------------------------------------------------------------------------------------")
+                for i in range(0, len(found)):
+                     print(f"{libraries[i]:10}\t{titles[i]:<32}\t{authors[i]:30}\t{genres[i]:30}\t{pages[i]:5}\t{statuses[i]:10}")
+                
+                print("--------------------------------------------------------------------------------------------------------------------------------------------\n")      
+    elif choice == "4":
+            genre = input("Enter the Genre you are looking for: ")
+            dex = search(genres, genre)
+            if dex:
+                display(dex)
+            else:
+                print("No results found!")
+    elif choice == "5":
+            library = input("Enter the Library Number you are looking for: ")
+            dex = search(libraries, library)
+            if dex:
+                display(dex)
+            else:
+                print("No results found!")
+    elif choice == "6":
+            dex = search(statuses, "available")
+            display(dex)
+    elif choice == "7":
+            dex = search(statuses, "on loan")
+    elif choice == "8":
+            print("Goodbye!")
+            ans = "x"
+    else:
+            print("Invalid choice!")
